09202d3806b0bd44c79acf3c4b02a0dd74af79f6
Václav Kavalír <kavalirv@natur.cuni.cz>
2019-11-05 18:19:25 +0100

    vyreseno meritko a odsazeni

--- ukol.py
+++ ukol.py
@@ -1,42 +1,42 @@
 from math import sin, tan, cos, log
-L=str #Lambert
-A=str #Marin
-B=str #Braun
-M=str #Mercator
-R=float(6371.11)
-v=float # zem.délka
-u=float # zem.šířka
-e=float(2.71828)
+L = str  # Lambert
+A = str  # Marin
+B = str  # Braun
+M = str  # Mercator
+R = float(6371.11)
+v = int  # zem.délka
+u = int  # zem.šířka
+e = float(2.71828)
 w = float(cos(u/2)/sin(u/2))  # cotanges pro Mercatora
-Poledniky1=float
-Rovnobezky1=float
-zobrazeni=input("Zadej typ zobrazení")
-if zobrazeni=='L':
-    for v in range(-180,180,10):
-        Poledniky1=R*v
-    for u in range(-90,90,10):
-        Rovnobezky1=R*sin(u)
-elif zobrazeni=='A':
+Poledniky1 = float
+Rovnobezky1 = float
+Poledniky = float
+Rovnobezky = float
+zobrazeni = str(input("Zadej typ zobrazení"))
+if zobrazeni == 'L':
     for v in range(-180, 180, 10):
-        Poledniky1=R*v
-    for u in range(-90,90,10):
-        Rovnobezky1=R*u
-elif zobrazeni=='B':
-    for v in range(-180,180,10):
-        Poledniky1=R*v
-    for u in range(-90,90,10):
-        Rovnobezky1=2*R*tan(u/2)
-elif zobrazeni=='M':
-    for v in range(-180, 180, 10):
-        Poledniky1=R*v
+        Poledniky1 = R*v
     for u in range(-90, 90, 10):
-        Rovnobezky1=R*math.log(w,e)
+        Rovnobezky1 = R*sin(u)
+elif zobrazeni == 'A':
+    for v in range(-180, 180, 10):
+        Poledniky1 = R*v
+    for u in range(-90, 90, 10):
+        Rovnobezky1 = R*u
+elif zobrazeni == 'B':
+    for v in range(-180, 180, 10):
+        Poledniky1 = R*v
+    for u in range(-90, 90, 10):
+        Rovnobezky1 = 2*R*tan(u/2)
+elif zobrazeni == 'M':
+    for v in range(-180, 180, 10):
+        Poledniky1 = R*v
+    for u in range(-90, 90, 10):
+        Rovnobezky1 = R*log(w, e)
 else:
     print("Zadej správné zobrazení")
-x=input("Zadej měřítko")
-Rovnobezky=Rovnobezky1/x
-Poledniky=Poledniky1/x
+x = float(input("Zadej měřítko"))
+Rovnobezky = Rovnobezky1 / x
+Poledniky = Poledniky1 / x
 print(Rovnobezky)
 print(Poledniky)
-
-
