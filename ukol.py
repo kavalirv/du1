b4a1641324ae20faaa8a978ad4c13d6980260a63
Václav Kavalír <kavalirv@natur.cuni.cz>
2019-11-05 22:05:06 +0100

    konecne jsem tam dostal seznam

--- ukol.py
+++ ukol.py
@@ -7,17 +7,20 @@ R = float(6371.11)
 v = int  # zem.délka
 u = int  # zem.šířka
 e = float(2.71828)
-w = float(cos(u/2)/sin(u/2))  # cotanges pro Mercatora
 Poledniky1 = float
 Rovnobezky1 = float
 Poledniky = float
 Rovnobezky = float
+result = []
+result1 = []
 zobrazeni = str(input("Zadej typ zobrazení"))
 if zobrazeni == 'L':
     for v in range(-180, 180, 10):
         Poledniky1 = R*v
+        result.append(float(Poledniky1))
     for u in range(-90, 90, 10):
         Rovnobezky1 = R*sin(u)
+        result1.append(float(Rovnobezky1))
 elif zobrazeni == 'A':
     for v in range(-180, 180, 10):
         Poledniky1 = R*v
@@ -38,5 +41,5 @@ else:
 x = float(input("Zadej měřítko"))
 Rovnobezky = Rovnobezky1 / x
 Poledniky = Poledniky1 / x
-print(Rovnobezky)
-print(Poledniky)
+print(result)
+print(result1)
