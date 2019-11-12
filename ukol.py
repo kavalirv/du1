a4854dc29c0ebc14676b3ed79a2fd14fdc5e7b69
Václav Kavalír <kavalirv@natur.cuni.cz>
2019-11-12 13:43:35 +0100

    Zjednoduseni poledniku a meritka

--- ukol.py
+++ ukol.py
@@ -13,32 +13,31 @@ Poledniky = float
 Rovnobezky = float
 result = []
 result1 = []
-zobrazeni = str(input("Zadej typ zobrazení"))
-if zobrazeni == 'L':
-    x = float(input("Zadej měřítko"))
+def vypocet_poledniku():
     for v in range(-180, 180, 10):
-        Poledniky1 = R*radians(v)  # poledniky po vypoctu
+        Poledniky1 = R * radians(v)  # poledniky po vypoctu
         Poledniky2 = Poledniky1 / x  # vydeleno meritkem
-        Poledniky3 = round(Poledniky2)  # zaokrouhleni na milimetry
+        Poledniky3 = round(Poledniky2, 1)  # zaokrouhleni na milimetry
         result.append(float(Poledniky3))  # vysledek vydeleny meritkem add to seznam
+zobrazeni = str(input("Zadej typ zobrazení"))
+x = float(input("Zadej měřítko"))
+if zobrazeni == 'L':
+    vypocet_poledniku()
     for u in range(-90, 90, 10):
         Rovnobezky1 = R*sin(radians(u))
         Rovnobezky2 = Rovnobezky1 / x
-        Rovnobezky3 = round(Rovnobezky2)
+        Rovnobezky3 = round(Rovnobezky2,1)
         result1.append(float(Rovnobezky3))
 elif zobrazeni == 'A':
-    for v in range(-180, 180, 10):
-        Poledniky1 = R*v
+    vypocet_poledniku()
     for u in range(-90, 90, 10):
         Rovnobezky1 = R*u
 elif zobrazeni == 'B':
-    for v in range(-180, 180, 10):
-        Poledniky1 = R*v
+    vypocet_poledniku()
     for u in range(-90, 90, 10):
         Rovnobezky1 = 2*R*tan(u/2)
 elif zobrazeni == 'M':
-    for v in range(-180, 180, 10):
-        Poledniky1 = R*v
+    vypocet_poledniku()
     for u in range(-90, 90, 10):
         Rovnobezky1 = R*log(atan(u/2), e)
 else:
